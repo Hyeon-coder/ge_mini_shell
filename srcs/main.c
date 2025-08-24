d3c60a2ba6ed28cc1135863b9aa187d8619f14dd
juhyeonl <juhyeonl@student.hive.fi>
2025-08-24 17:59:24 +0300

    add

--- srcs/main.c
+++ srcs/main.c
@@ -4,7 +4,7 @@
 /* main.c                                             :+:      :+:    :+:   */
 /* +:+ +:+         +:+     */
 /* By: juhyeonl <juhyeonl@student.42.fr>          +#+  +:+       +#+        */
-/* +#+#+#+#+#+   +#+           */
+/*+#+#+#+#+#+   +#+           */
 /* Created: 2025/08/24 11:27:00 by your_login        #+#    #+#             */
 /* Updated: 2025/08/24 19:30:00 by juhyeonl         ###   ########.fr       */
 /* */
@@ -18,11 +18,19 @@ static void	execute_line(char *line, t_shell *shell)
 {
 	t_token		*tokens;
 	t_ast_node	*ast;
+	char		*trimmed_line;
 
-	tokens = lexer(line);
+	trimmed_line = ft_strtrim(line, " \t");
+	if (!trimmed_line || !*trimmed_line)
+	{
+		free(trimmed_line);
+		return ;
+	}
+	tokens = lexer(trimmed_line);
 	if (!tokens)
 	{
 		shell->last_exit_status = g_exit_status;
+		free(trimmed_line);
 		return ;
 	}
 	ast = parser(&tokens);
@@ -30,12 +38,14 @@ static void	execute_line(char *line, t_shell *shell)
 	{
 		cleanup(tokens, NULL);
 		shell->last_exit_status = g_exit_status;
+		free(trimmed_line);
 		return ;
 	}
 	expand_ast(ast, shell);
 	shell->last_exit_status = executor(ast, shell);
 	g_exit_status = shell->last_exit_status;
 	cleanup(tokens, ast);
+	free(trimmed_line);
 }
 
 void	shell_loop(t_shell *shell)
@@ -68,20 +78,13 @@ void	shell_loop(t_shell *shell)
 void	non_interactive_mode(t_shell *shell)
 {
 	char	*line;
-	char	*trimmed_line;
 
-	while (1)
+	line = get_next_line(STDIN_FILENO);
+	while (line)
 	{
-		line = get_next_line(STDIN_FILENO);
-		if (!line)
-			break ;
-		trimmed_line = ft_strtrim(line, "\n");
+		execute_line(line, shell);
 		free(line);
-		if (trimmed_line && *trimmed_line)
-		{
-			execute_line(trimmed_line, shell);
-		}
-		free(trimmed_line);
+		line = get_next_line(STDIN_FILENO);
 	}
 }
 
