8182bcbed0eff273fecd1920dfbc555346b8164a
juhyeonl <juhyeonl@student.hive.fi>
2025-08-30 02:15:25 +0300

    heredoc 2

--- src/execution/heredoc.c
+++ src/execution/heredoc.c
@@ -1,26 +1,44 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   heredoc.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: JuHyeon <JuHyeon@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/29 17:53:24 by JuHyeon           #+#    #+#             */
-/*   Updated: 2025/08/30 00:51:02 by JuHyeon          ###   ########.fr       */
-/*                                                                            */
+/* */
+/* :::      ::::::::   */
+/* heredoc.c                                          :+:      :+:    :+:   */
+/* +:+ +:+         +:+     */
+/* By: JuHyeon <JuHyeon@student.42.fr>            +#+  +:+       +#+        */
+/* +#+#+#+#+#+   +#+           */
+/* Created: 2025/08/29 17:53:24 by JuHyeon           #+#    #+#             */
+/* Updated: 2025/08/30 03:30:00 by JuHyeon          ###   ########.fr       */
+/* */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/*
+** An event hook function for readline.
+** It is called periodically while readline is waiting for input.
+** Returns 1 to terminate readline if a SIGINT signal has been caught.
+*/
+static int	heredoc_event_hook(void)
+{
+	if (g_signal == SIGINT)
+		return (1);
+	return (0);
+}
+
+/*
+** Signal handler for SIGINT during heredoc input.
+** This function is kept simple and only sets a global flag,
+** which is a safe operation within a signal handler.
+*/
 void	heredoc_sigint_handler(int sig)
 {
 	(void)sig;
 	g_signal = SIGINT;
-	rl_on_new_line();
-	rl_replace_line("", 0);
-    rl_redisplay();
 }
 
+/*
+** Writes a single line to the heredoc's temporary file.
+** It expands variables within the line if it was not quoted.
+*/
 static void	write_to_heredoc(t_ms *ms, char *line, int fd, int expand)
 {
 	char	*expanded_line;
@@ -38,6 +56,11 @@ static void	write_to_heredoc(t_ms *ms, char *line, int fd, int expand)
 	free(line);
 }
 
+/*
+** The main loop for reading heredoc input from the user.
+** It sets up a readline event hook to immediately catch Ctrl+C interruptions
+** without requiring an extra key press.
+*/
 static int	heredoc_loop(t_ms *ms, char *lim, int fd, int quo)
 {
 	char				*line;
@@ -45,6 +68,7 @@ static int	heredoc_loop(t_ms *ms, char *lim, int fd, int quo)
 	struct termios		orig_termios;
 
 	setup_heredoc_handlers(&orig_termios, &sa_old);
+	rl_event_hook = heredoc_event_hook;
 	while (1)
 	{
 		line = readline("> ");
@@ -55,6 +79,7 @@ static int	heredoc_loop(t_ms *ms, char *lim, int fd, int quo)
 		}
 		write_to_heredoc(ms, line, fd, (quo == UNQUOTED));
 	}
+	rl_event_hook = NULL;
 	restore_handlers(&orig_termios, &sa_old);
 	if (g_signal == SIGINT)
 	{
@@ -64,6 +89,10 @@ static int	heredoc_loop(t_ms *ms, char *lim, int fd, int quo)
 	return (0);
 }
 
+/*
+** Initializes the heredoc process. It creates a temporary file,
+** runs the input loop, and finalizes the redirection.
+*/
 int	start_heredoc(t_ms *ms, char *lim, t_infile *infile, int quo)
 {
 	int		fd;
